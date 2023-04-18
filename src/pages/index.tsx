d581f813aa59359b438d563155c0ec6f5111b63f
Alexia Aldea <alexia.elena.aldea@gmail.com>
2023-04-18 13:37:59 +0300

    chore: creating a basic layout for primary application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { styled } from '@mui/system';
 import { Typography } from '@mui/material';
+import NavigationLayout from '@/components/Layouts/NavigationLayout';
 
 const StyledTypography = styled(Typography)(
   ({ theme }) => `
@@ -12,6 +13,10 @@ const StyledTypography = styled(Typography)(
 );
 const Index = () => {
   return <StyledTypography>Hello Next</StyledTypography>;
+};
+
+Index.getLayout = function getLayout(page: React.ReactElement) {
+  return <NavigationLayout>{page}</NavigationLayout>;
 };
 
 export default Index;
