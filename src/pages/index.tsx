cbf4dede136d9dc548da236d2c038d5b1fdb084b
Alexia Aldea <94163344+allee15@users.noreply.github.com>
2023-05-07 12:22:32 +0300

    chore: update UX for main page (#16)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { styled } from '@mui/system';
 import { Typography } from '@mui/material';
 import NavigationLayout from '@/components/Layouts/NavigationLayout';
-import BasicAlert from '@/components/Alerts/BasicAlert';
+import AuthHeader from '@/utils/authrorizationHeader';
+import { useRouter } from 'next/navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import { accountActions } from '@/store/reducers/account';
 
 const StyledTypography = styled(Typography)(
-  ({ theme }) => `
-  margin-left: 10px;
-  color: ${theme.palette.colorText.alternative};
-  
-  font-size: 30px;
-`
+  ({ theme }) =>
+    `margin-left: 10px;
+color: ${theme.palette.colorText.alternative};
+
+font-size: 30px;`
 );
 const Index = () => {
-  const [open, setOpen] = useState(true);
+  const router = useRouter();
+  const username = useRef<string>();
+
+  useEffect(() => {
+    if (window.sessionStorage) {
+      if (Object.keys(AuthHeader()).length === 0) {
+        router.push('/login');
+      } else {
+        username.current = sessionStorage.getItem('username');
+      }
+    }
+  }, []);
   return (
     <>
-      <StyledTypography>Hello Next</StyledTypography>
-      <BasicAlert open={open} setOpen={setOpen} />
+      <StyledTypography>Hello {username.current || 'Next'}</StyledTypography>
     </>
   );
 };
